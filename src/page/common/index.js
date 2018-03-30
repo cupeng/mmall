c4e7a0c1fa6f76465ba74ab7dbad98c42486af4b
cupeng <872345732@qq.com>
2018-03-30 21:01:59 +0800

    new file

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -1,4 +1,8 @@
 'use strict'
 
 import './index.css'
+import 'page/common/nav/index.js'
+import 'page/common/nav-simple/index.js'
+import 'page/common/footer/index.css'
 import 'node_modules/font-awesome/css/font-awesome.min.css'
+
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
 import './index.css'
-import 'page/common/nav-simple/index.js'
 import _mm from 'util/mm.js'
 
